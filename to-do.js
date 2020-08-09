a759c82bf0a642858dcd5a8aacf7ea5931531cea
Nahla-a <nahla.abuelela@gmail.com>
2020-08-09 05:51:43 -0500

    updating my code

--- to-do.js
+++ to-do.js
@@ -1,11 +1,64 @@
-var addButton = document.getElementById("add").addEventListener('click', buttonClick)
+const addButton = document.getElementById("add").addEventListener('click', addTask)
 
-function buttonClick(){
+function addTask() {
     // console.log('test');
-    var addNewItem = document.getElementById("newItem").value ;
-    var text = document.createTextNode(addNewItem)
-    item1.appendChild(text)
-    document.getElementById("theForm").appendChild(item1)
+    let addNewItem = document.getElementById("newItem").value;
+    let form = document.getElementById("theForm");
+    console.log("addNewItem");
 
+    let tasklistItem, taskcheckBox, taskLabel, taskTrashButton, newLine;
+
+    // Create a list:
+    taskListItem = document.createElement("li");
+    taskListItem.id = "item1";
+
+    // Create checkbox:
+    taskcheckBox = document.createElement("input");
+    taskcheckBox.type = "checkbox";
+    taskcheckBox.id = "check";
+
+    // Create a label:
+    taskLabel = document.createElement("label");
+    taskLabel.htmlFor = "check";
+    textLabel = document.createTextNode("");
+    taskLabel.appendChild(textLabel);
+
+    // Create trash button:
+    taskTrashButton = document.createElement("button");
+    textTrashButton = document.createTextNode("Clear");
+    taskTrashButton.appendChild(textTrashButton);
+
+    // Create a newLine:
+    space = document.createElement("BR");
+
+    // Appending label and checkbox into the list:
+    taskListItem.appendChild(taskcheckBox);
+    taskListItem.appendChild(taskLabel);
+
+    // Appending the new item:
+    taskListItem.appendChild(document.createTextNode(addNewItem));
+    form.appendChild(taskListItem);
+
+    // Appending trash into the list:
+    taskListItem.appendChild(taskTrashButton);
+
+
+    // Appending a new line:
+    form.appendChild(space);
 
 }
+
+function createDeleteAllButton() {
+    let mainContainer = document.getElementById("container");
+    let taskDeleteAll;
+
+    // Create a delete all button:
+    taskDeleteAll = document.createElement("button");
+    textDeleteAllButton = document.createTextNode("DELETE");
+    taskDeleteAll.appendChild(textDeleteAllButton);
+
+    // Appending the delete-all button to the form:
+    mainContainer.appendChild(taskDeleteAll);
+}
+createDeleteAllButton();
+
